7cd672497f413d14e8935942bc1fa1c5941a9991
Ghilain <ishimweghilain5050@gmail.com>
2022-09-16 12:54:13 +0200

    Add test

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,18 +1,19 @@
 class GeneralShoppingListController < ApplicationController
-  def index
-    user_foods = current_user.foods
-    all_foods = current_user.recipes.includes(recipe_foods: [:food]).map do |recipe|
-      recipe.recipe_foods.map(&:food)
+    def index
+      user_foods = current_user.foods
+      all_foods = current_user.recipes.includes(recipe_foods: [:food]).map do |recipe|
+        recipe.recipe_foods.map(&:food)
+      end
+      recipes_foods = all_foods.flatten
+      @food_items_to_buy = user_foods.select { |food| food unless recipes_foods.include?(food) }
+      @total_foods = @food_items_to_buy.count
+      @total_price = @food_items_to_buy.map(&:price).sum
     end
-    recipes_foods = all_foods.flatten
-    @food_items_to_buy = user_foods.select { |food| food unless recipes_foods.include?(food) }
-    @total_foods = @food_items_to_buy.count
-    @total_price = @food_items_to_buy.map(&:price).sum
+  
+    private
+  
+    def recipe_params
+      params.require(:recipe).permit(:recipe)
+    end
   end
-
-  private
-
-  def recipe_params
-    params.require(:recipe).permit(:recipe)
-  end
-end
+  
