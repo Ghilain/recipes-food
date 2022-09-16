283938587056d1dd430674d23b4aeac30a4988b2
Ghilain <ishimweghilain5050@gmail.com>
2022-09-16 15:13:23 +0200

    Make changes and fix some errors

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,38 +1,42 @@
 class RecipeFoodsController < ApplicationController
-  #  before_action :set_recipe_food, only: %i[show edit update destroy]
+  #  before_action :set_recipe_food
   skip_before_action :verify_authenticity_token
-  # GET /recipe_foods or /recipe_foods.json
+  # Show all data:
+  # /recipe_foods or /recipe_foods.json
   def index
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_foods = RecipeFood.where(recipe_id: @recipe.id)
   end
 
-  # GET /recipe_foods/1 or /recipe_foods/1.json
+  # Show data according id:
+  # /1 or /1.json
   def show
     @recipe = Recipe.find(params[:id])
     @recipe_food = RecipeFood.find(params[:id])
   end
 
-  # GET /recipe_foods/new
+  # New recipe food by /new
   def new
     @recipe_food = RecipeFood.new
     @recipe = Recipe.find(params[:recipe_id])
   end
 
-  # GET /recipe_foods/1/edit
+  # Edit recipe food according to it it 
+  # /1/edit
   def edit
     @recipe = Recipe.find(params[:id])
     @recipe_food = RecipeFood.find(params[:id])
   end
 
-  # POST /recipe_foods or /recipe_foods.json
+  # Create new recipe food:
+  # /recipe_foods or /recipe_foods.json
   def create
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_food = @recipe.recipe_foods.new(recipe_food_params)
 
     respond_to do |format|
       if @recipe_food.save
-        format.html { redirect_to recipe_url(@recipe.id), notice: 'Recipe food was successfully created.' }
+        format.html { redirect_to recipe_url(@recipe.id), notice: 'Recipe food created successfully.' }
         format.json { render :show, status: :created, location: @recipe }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -41,21 +45,19 @@ class RecipeFoodsController < ApplicationController
     end
   end
 
-  # DELETE /recipe_foods/1 or /recipe_foods/1.json
+  # Delete recipe food by it id
+  # /1 or /1.json
   def destroy
     @recipe_food = RecipeFood.find(params[:id])
     @recipe_food.destroy
 
     respond_to do |format|
-      format.html { redirect_to recipes_path, notice: 'Recipe food was successfully destroyed.' }
+      format.html { redirect_to recipes_path, notice: 'Recipe food destroyed .' }
       format.json { head :no_content }
     end
   end
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-
   # Only allow a list of trusted parameters through.
   def recipe_food_params
     params.require(:recipe_food).permit(:food_id, :quantity)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,28 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  # before_action :set_user, only: %i[show edit update destroy]
-
-  # GET /users or /users.json
+  # Shwo all users:
+  # /users or /users.json
   def index
     @users = User.all
   end
 
-  # GET /users/1 or /users/1.json
+  # Show user by their id:
+  # /1 or /1.json
   def show
     @user = User.find(params[:id])
   end
 
-  # GET /users/new
+  # Create new user/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
+  # Edit use by their id
+  # /1/edit
   def edit; end
 
-  # POST /users or /users.json
+  # Create new user:
+  # /users or /users.json
   def create
     @user = User.new(user_params)
 
@@ -34,38 +36,19 @@ class UsersController < ApplicationController
       end
     end
   end
-
-  # # PATCH/PUT /users/1 or /users/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @user.update(user_params)
-  #       format.html { redirect_to user_url(@user), notice: 'User was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @user }
-  #     else
-  #       format.html { render :edit, status: :unprocessable_entity }
-  #       format.json { render json: @user.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # DELETE /users/1 or /users/1.json
+  # Delete user by their id
+  # /1 or /1.json
   def destroy
     @user.destroy
 
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to users_url, notice: 'User successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  # def set_user
-  #   @user = User.find(params[:id])
-  # end
-
-  # Only allow a list of trusted parameters through.
   def user_params
     params.require(:user).permit(:name)
   end
